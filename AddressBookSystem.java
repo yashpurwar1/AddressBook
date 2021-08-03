0f43b01b28889d8cf850ae1cfcf408d5b9ad40b1
yashpurwar1 <yashpurwar.yashpurwar.com>
2021-08-03 17:52:29 +0530

    Added: Functionality to add multiple new contact

--- AddressBookSystem.java
+++ AddressBookSystem.java
@@ -7,27 +7,38 @@ import java.util.Scanner;
 public class AddressBookSystem {
     static Scanner sc = new Scanner(System.in);
     private List<Person> personList = new ArrayList<>();
-    /*
-     * UC1 Taking User Input for person details
-     */
+
+    // UC1 Taking User Input for person detail
+    // UC2 this method adds object person and its element to ArrayList
+
     private void addNewContact() {
         Person person = new Person();
         System.out.println("Enter First Name");
         String firstName = sc.next();
+        person.setFirstName(firstName);
         System.out.println("Enter last Name");
         String lastName = sc.next();
+        person.setLastName(lastName);
         System.out.println("Enter the Address");
         String address = sc.next();
+        person.setAddress(address);
         System.out.println("Enter the City");
         String city = sc.next();
+        person.setCity(city);
         System.out.println("Enter the State");
         String state = sc.next();
+        person.setState(state);
         System.out.println("Enter the PinCode");
         String pinCode = sc.next();
+        person.setPinCode(pinCode);
         System.out.println("Enter the Phone Number");
         String phoneNumber = sc.next();
+        person.setPhoneNumber(phoneNumber);
         System.out.println("Enter the Email");
         String emailId = sc.next();
+        person.setEmailId(emailId);
+
+        personList.add(person);
     }
 
     public static void main(String[] args) {
